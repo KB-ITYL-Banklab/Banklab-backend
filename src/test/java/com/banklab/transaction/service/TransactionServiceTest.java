cc5f78b55d7c704af78c95f85c0a5c37dc4c6ef0
SHon'Son <sow7722@gmail.com>
2025-07-16 11:40:43 +0900

    [BNK-21] Test: 일별 지출 조회 기능 테스트

--- src/test/java/com/banklab/transaction/service/TransactionServiceTest.java
+++ src/test/java/com/banklab/transaction/service/TransactionServiceTest.java
@@ -4,6 +4,7 @@ import com.banklab.codeapi.config.RestTemplateConfig;
 import com.banklab.config.RootConfig;
 import com.banklab.config.ServletConfig;
 import com.banklab.config.WebConfig;
+import com.banklab.transaction.dto.DailyExpenseDTO;
 import com.banklab.transaction.dto.MonthlySummaryDTO;
 import com.banklab.transaction.mapper.TransactionMapper;
 import lombok.extern.log4j.Log4j2;
@@ -16,6 +17,8 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -33,14 +36,16 @@ class TransactionServiceTest {
     @Autowired
     private TransactionMapper  transactionMapper;
 
+    String ExistentAccount = "110568497184";
+    String nonExistentAccount = "0000000000";
+
+
     @Test
     @DisplayName("월간 요약 정보 조회 테스트 - DB에 데이터가 없는 경우")
     void getMonthlySummary_whenNoDataInDB() {
         // given
         int year = 2024;
         int month = 7;
-        // DB에 존재하지 않는 임의의 계좌번호
-        String nonExistentAccount = "0000000000";
 
         // when
         MonthlySummaryDTO actualSummary = transactionService.getMonthlySummary(year, month, nonExistentAccount);
@@ -57,7 +62,6 @@ class TransactionServiceTest {
         // given
         int year = 2025;
         int month = 7;
-        String ExistentAccount = "110568497184";
 
         long expectedTotalIncome = 870666L;
         long expectedTotalOutcome = 716479L;
@@ -67,5 +71,30 @@ class TransactionServiceTest {
 
         assertEquals(expectedTotalOutcome, monthlySummary.getTotalExpense());
         assertEquals(expectedTotalIncome, monthlySummary.getTotalIncome());
+    }
+
+    @Test
+    @DisplayName("일 별 지출 정보 조회 테스트 - DB 데이터 존재")
+    void getDailyExpense_whenDataInDB() {
+        // given
+        int year = 2025;
+        int month = 6;
+
+        //when
+        List<DailyExpenseDTO> dailyExpense = transactionService.getDailyExpense(year, month, ExistentAccount);
+        assertNotNull(dailyExpense);
+    }
+
+    @Test
+    @DisplayName("일 별 지출 정보 조회 테스트 - DB 데이터 미존재")
+    void getDailyExpense_whenNoDataInDB() {
+        // given
+        int year = 2025;
+        int month = 7;
+
+        //when
+        List<DailyExpenseDTO> dailyExpense = transactionService.getDailyExpense(year, month, nonExistentAccount);
+
+        assertEquals(dailyExpense.size(), 0);
     }
 }
