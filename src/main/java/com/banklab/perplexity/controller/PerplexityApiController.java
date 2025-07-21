ec6d2e74488222cd23716ad6a917d565908ce276
SHon'Son <sow7722@gmail.com>
2025-07-21 09:34:48 +0900

    [BNK-55] Feat: Perplexity API 연동

--- src/main/java/com/banklab/perplexity/controller/PerplexityApiController.java
+++ src/main/java/com/banklab/perplexity/controller/PerplexityApiController.java
@@ -3,6 +3,7 @@ package com.banklab.perplexity.controller;
 import com.banklab.perplexity.dto.PerplexityRequestDto;
 import com.banklab.perplexity.dto.PerplexityResponseDto;
 import com.banklab.perplexity.service.PerplexityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/perplexity")
+@RequiredArgsConstructor
 public class PerplexityApiController {
 
     private final PerplexityService perplexityService;
-
-    public PerplexityApiController(PerplexityService perplexityService) {
-        this.perplexityService = perplexityService;
-    }
 
     @PostMapping("/chat")
     public PerplexityResponseDto chat(@RequestBody PerplexityRequestDto requestDto) {
