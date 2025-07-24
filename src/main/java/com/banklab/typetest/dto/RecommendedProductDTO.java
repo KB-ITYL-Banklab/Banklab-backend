3d37e323211e43e37c7a2b70361d805e3f6e7d32
2uwls <2uwls1126@gmail.com>
2025-07-25 04:22:57 +0900

    [BNK-88] Feat: 추천상품 반환값 수정

--- src/main/java/com/banklab/typetest/dto/RecommendedProductDTO.java
+++ src/main/java/com/banklab/typetest/dto/RecommendedProductDTO.java
@@ -25,4 +25,9 @@ public class RecommendedProductDTO {
     private String interestRate;    // 금리 정보
     private String productFeature; // 상품 특징
     private String targetCustomer; // 대상 고객
+
+    //상품 조회용 필드
+    private String dclsMonth;   // 공시월
+    private String finCoNo;     // 금융회사 코드
+    private String finPrdtCd;
 }
