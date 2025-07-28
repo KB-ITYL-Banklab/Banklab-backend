653b273d81447d1b5da60f66b4529a9ed7867da2
dudujh0106 <rkdwlgh01@naver.com>
2025-07-29 00:22:35 +0900

    [BNK-103] Feat: 이메일 인증 로직에 중복 검증 추가

--- src/main/java/com/banklab/verification/email/controller/EmailVerificationController.java
+++ src/main/java/com/banklab/verification/email/controller/EmailVerificationController.java
@@ -1,6 +1,7 @@
 package com.banklab.verification.email.controller;
 
 import com.banklab.common.response.StatusResponse;
+import com.banklab.member.service.MemberService;
 import com.banklab.verification.email.dto.EmailSendDTO;
 import com.banklab.verification.email.dto.EmailVerifyDTO;
 import com.banklab.verification.email.service.EmailVerificationService;
@@ -18,10 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailVerificationController {
 
     private final EmailVerificationService emailService;
+    private final MemberService memberService;
 
     @PostMapping("/send")
     @ApiOperation(value = "인증번호 전송")
     public ResponseEntity<StatusResponse> sendCode(@RequestBody EmailSendDTO request) {
+        if (memberService.checkDuplicate(request.getEmail())) {
+            return ResponseEntity.status(409).body(new StatusResponse(false,"이미 가입된 이메일입니다."));
+        }
         emailService.sendVerificationCode(request.getEmail());
         return ResponseEntity.ok(new StatusResponse(true,"이메일 전송 완료"));
     }
